0aadfbf02cff800eb452ddd241d893adc2671e32
soyun2 <tkdtod0619@naver.com>
2021-02-09 12:48:38 +0900

    .

---
+++ 20210209/src/Ex01.java
@@ -0,0 +1,14 @@
+
+public class Ex01 {
+	public static void main(String[] args) {
+		String a = "123";
+		
+		String b = "abc";
+		
+		int c = Integer.parseInt(a);
+		double d = Double.parseDouble(a);
+		
+		System.out.println("c = "+c);
+		System.out.println("d = "+d);
+	}
+}
